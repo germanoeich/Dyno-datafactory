236e1d62c912bd5fade8b2116737258c0c71709f
Brian Tanner <brian@tanner.io>
2016-12-03 03:39:07 +0700

    changes to modlog schema

--- models/Modlog.js
+++ models/Modlog.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const modLogSchema = new Schema({
+  caseNum:     { type: Number, index: true },
   server:      { type: String, required: true, index: true },
   user:        { type: Object, required: true },
   mod:         { type: Object, required: false },
   type:        { type: String, required: true },
   reason:      { type: String },
+  message:     { type: String },
   createdAt:   { type: Date, default: Date.now, expires: '30d' },
-});
+}, { strict: false });
 
 module.exports = mongoose.model('ModLog', modLogSchema);
